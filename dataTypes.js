e6b891db7aa5aecc48bb220625dc4f2a0a865993
MinuPriya9 <89161078+MinuPriya9@users.noreply.github.com>
2023-09-26 13:47:29 +0530

    deleted a file

---
+++ dataTypes.js
@@ -0,0 +1,10 @@
+console.log(10 + "10"); //concate
+
+console.log(10 - "5"); //bug
+
+console.log("Minu " + "Priya");
+
+console.log("Minu " - "Priya"); //not a number
+
+console.log(true + true);
+
